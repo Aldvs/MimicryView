a4e861893f912a6c23d1cbd4b441e63821dde10d
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-19 00:49:09 +0300

    Update MimicryView

--- MimicryView/ContentView.swift
+++ MimicryView/ContentView.swift
@@ -8,17 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isExtended: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            HStack {
+                if !isExtended {
+                    Spacer()
+                }
+                ZStack {
+                    Rectangle()
+                        .fill(Color.blue)
+                        .frame(width: isExtended ? .infinity : 150, height: isExtended ? 400 : 75)
+                        .cornerRadius(25)
+                    VStack {
+                        HStack {
+                            ButtonView(isExtended: $isExtended)
+                            if isExtended {
+                                Spacer()
+                            }
+                        }
+                        if isExtended {
+                            Spacer()
+                        }
+                    }
+                }
+                .frame(width: isExtended ? .infinity : 150, height: isExtended ? 400 : 75)
+                .padding(.leading, isExtended ? 30 : 0)
+            }
+            .padding(.trailing, 30)
+            .padding(.bottom, 30)
+            if isExtended {
+                Spacer()
+            }
         }
-        .padding()
+        .animation(.easeOut, value: isExtended)
+    }
+}
+
+struct ButtonView: View {
+    @Binding var isExtended: Bool
+    
+    var body: some View {
+        Button(action: {
+            withAnimation(.spring) {
+                isExtended.toggle()
+            }
+            print("Right upper corner pressed")
+        }) {
+            HStack {
+                if isExtended {
+                    Image(systemName: "arrowshape.backward.fill")
+                        .foregroundColor(.white)
+                }
+                Text(isExtended ? "Back" : "Open")
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+//                    .animation(nil, value: isExtended)
+            }
+        }
+        .frame(width: 125, height: 75)
     }
 }
 
 #Preview {
     ContentView()
 }
+
