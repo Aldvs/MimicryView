f20dae3a7a92b06fe918463eb5384f46c0d9c327
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-18 11:53:03 +0300

    Initial Commit

---
+++ MimicryView/MimicryViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimicryViewApp.swift
+//  MimicryView
+//
+//  Created by admin on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MimicryViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
